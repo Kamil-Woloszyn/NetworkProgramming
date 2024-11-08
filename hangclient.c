d31231e125d7bc06b96c5f62171a98391a5ffe24
Kamil Woloszyn <144813091+Kamil-Woloszyn@users.noreply.github.com>
2024-11-08 21:07:56 +0000

    Input fixing

--- hangclient.c
+++ hangclient.c
@@ -1,10 +1,17 @@
 /* Hangclient.c - Client for hangman server.  */
 
- #include <stdio.h>
  #include <sys/types.h>
  #include <sys/socket.h>
  #include <netinet/in.h>
+ #include <stdio.h>
+ #include <syslog.h>
+ #include <signal.h>
+ #include <errno.h>
+ #include <stdlib.h>
+ #include <string.h>
+ #include <unistd.h>
  #include <netdb.h>
+#include <ctype.h> 
 
  # define LINESIZE 80
  # define HANGMAN_TCP_PORT 1066
@@ -31,7 +38,7 @@
 
  	host_info = gethostbyname(server_name);
  	if (host_info == NULL) {
- 		fprintf (stderr, "%s: unknown host:%s \ n", argv [0], server_name);
+ 		fprintf (stderr, "%s: unknown host:%s \n", argv[0], server_name);
  		exit (2);
  	}
 
@@ -49,7 +56,7 @@
  Take a line from the server and show it, take a line and send the user input to the server. 
  Repeat until the server terminates the connection. */
 
- 	printf ("Connected to server% s \ n", server_name);
+ 	printf ("Connected to server %s \n", server_name);
  	while ((count = read (sock, i_line, LINESIZE)) > 0) {
  		write (1, i_line, count);
  		count = read (0, o_line, LINESIZE);//0 = STDIN
